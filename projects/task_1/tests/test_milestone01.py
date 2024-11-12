5ae074a41c0261908563e1c0819b51e3a5941155
Attila <kekesi.att@gmail.com>
2024-11-12 23:16:02 +0100

    update tests

--- projects/task_1/tests/test_milestone01.py
+++ projects/task_1/tests/test_milestone01.py
@@ -3,7 +3,8 @@ import unittest
 from task_1.main import main
 from task_1.milestone01 import milestone01_tasks
 from task_1.milestone01_task_1_3 import milestone01_task_1_3
-from task_1.milestone01_task_1_4 import milestone01_task_1_4, milestone01_task_1_4_3, milestone01_task_1_4_4
+from task_1.milestone01_task_1_4 import milestone01_task_1_4, milestone01_task_1_4_1, milestone01_task_1_4_2, milestone01_task_1_4_3
+from task_1.milestone01_task_1_5 import milestone01_task_1_5, milestone01_task_1_5_1, milestone01_task_1_5_2
 
 
 class TestMilestone01(unittest.TestCase):
@@ -46,17 +47,42 @@ class TestMilestone01(unittest.TestCase):
         milestone01_task_1_4()
         self.assertTrue(True)
 
+    def test_milestone_task_1_4_1(self):
+        """
+        """
+        milestone01_task_1_4_1()
+        self.assertTrue(True)
+
+    def test_milestone_task_1_4_2(self):
+        """
+        """
+        milestone01_task_1_4_2()
+        self.assertTrue(True)
+
     def test_milestone_task_1_4_3(self):
         """
         """
         milestone01_task_1_4_3()
         self.assertTrue(True)
 
-    def test_milestone_task_1_4_4(self):
+    def test_milestone_task_1_5(self):
         """
         """
-        milestone01_task_1_4()
+        milestone01_task_1_5()
         self.assertTrue(True)
+
+    def test_milestone_task_1_5_1(self):
+        """
+        """
+        milestone01_task_1_5_1()
+        self.assertTrue(True)
+
+    def test_milestone_task_1_5_2(self):
+        """
+        """
+        milestone01_task_1_5_2()
+        self.assertTrue(True)
+
 
 if __name__ == "__main__":
     unittest.main()
